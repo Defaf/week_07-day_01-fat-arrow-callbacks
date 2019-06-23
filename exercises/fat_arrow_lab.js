63b862bedb5a23a7b02f7917b578ca6f28f56d92
Defaf <defaf_kamfar@hotmail.com>
2019-06-23 12:33:55 +0300

    Finish Fat arrow excercise

--- exercises/fat_arrow_lab.js
+++ exercises/fat_arrow_lab.js
@@ -8,9 +8,14 @@ console.log('Is 2 even?', isEven(2))
 
 // 1. Rewrite the isEven function using arrow syntax.
 // LAB CODE GOES HERE
-
+const isEven =  (num) => {
+  return num % 2 === 0
+}
 // 2. Rewrite the isEven function using arrow syntax with implicit return.
 // LAB CODE GOES HERE
+const isEven =  (num) => num % 2 === 0
 
 // 3. Rewrite the isEven function using arrow syntax with implicit return and single parameter no paranthesis.
 // LAB CODE GOES HERE
+const isEven = num => num % 2 === 0
+
